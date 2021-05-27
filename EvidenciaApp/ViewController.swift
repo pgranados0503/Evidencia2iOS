585e40c6506b853e5cab322f7e812b9723b015ac
pgranados0503 <70778101+pgranados0503@users.noreply.github.com>
2021-05-27 16:20:21 -0500

    Add files via upload

---
+++ EvidenciaApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EvidenciaApp
+//
+//  Created by Paul Granados on 27/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
